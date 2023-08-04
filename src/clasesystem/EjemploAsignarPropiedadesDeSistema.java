1fce94ba808c3b26581ad10ae04ad1ff65c5e811
Wolfgang Nylander <wolfgang.nylander@solera.com>
2023-08-04 10:14:18 +0200

    Clase System y funcionalidades

---
+++ src/clasesystem/EjemploAsignarPropiedadesDeSistema.java
@@ -0,0 +1,25 @@
+package clasesystem;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class EjemploAsignarPropiedadesDeSistema {
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream archivo = new FileInputStream("src/clasesystem/config.properties");
+            Properties p = new Properties(System.getProperties());
+            p.load(archivo);
+            p.setProperty("mi.propiedad.personalizada", "Mi valor");
+
+            System.setProperties(p);
+            Properties ps = System.getProperties();
+            System.out.println("ps.getProperty(\"mi.propiedad.personalizada\") = " + ps.getProperty("mi.propiedad.personalizada"));
+            System.out.println(System.getProperty("otra"));
+
+            ps.list(System.out);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

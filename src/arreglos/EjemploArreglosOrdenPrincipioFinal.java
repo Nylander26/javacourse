309fe7c8d716628b639453ee136dd6cfa0a7c9df
Wolfgang Nylander <wolfgang.nylander@solera.com>
2023-08-08 12:33:28 +0200

    Arreglos

--- src/arreglos/EjemploArreglosOrdenPrincipioFinal.java
+++ src/arreglos/EjemploArreglosOrdenPrincipioFinal.java
@@ -12,8 +12,8 @@ public class EjemploArreglosOrdenPrincipioFinal {
 
         int aux = 0;
         for (int i = 0; i < numeros.length / 2; i++){
-            a[aux++] = numeros[i];
             a[aux++] = numeros[numeros.length - 1 - i];
+            a[aux++] = numeros[i];
 //            System.out.print(numeros[i] + " ");
 //            System.out.println(numeros[numeros.length - 1 - i]);
         }
